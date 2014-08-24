f158a6a1bb21ac3be8119c4218dca6b602a2c3b9
Otto Schnurr <otto.schnurr@gmail.com>
2014-08-23 22:24:23 -0400

    Add empty Cocoa Framework project.
    Decorate source with MIT license.

---
+++ FileInput/FileInput.h
@@ -0,0 +1,22 @@
+//
+//  FileInput.h
+//
+//  Created by Otto Schnurr on 8/23/2014.
+//  Copyright (c) 2014 Otto Schnurr. All rights reserved.
+//
+//  MIT License
+//     file: ../../LICENSE.txt
+//     http://opensource.org/licenses/MIT
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FileInput.
+FOUNDATION_EXPORT double FileInputVersionNumber;
+
+//! Project version string for FileInput.
+FOUNDATION_EXPORT const unsigned char FileInputVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileInput/PublicHeader.h>
+
+
